03945aa474555df23ea38e3ad49c09185c9f7eef
Myra Jarenga <105559715+myrajarenga@users.noreply.github.com>
2023-03-11 07:38:00 +0000

    how to use IF statement

---
+++ if_awkward.py
@@ -0,0 +1,7 @@
+# Use an if statement to print "Leave!" if the user responds with 'yes'
+is_awkward = input("Is the party awkward?: ")
+# add your if statement here
+# ...
+
+if is_awkward.lower() == "yes":
+  print("leave!")
